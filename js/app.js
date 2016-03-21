6642521c75eb24f82baac2a7c135a12a0a7d505c
Daniel Guajardo Kushner <daniel.guajardo.k@gmail.com>
2016-03-21 16:00:36 -0300

    * JSON Data

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 (function($){
 
-	var Dentalink = {
+	var Schedule = {
 		variables: {
 			step: 30, //minutes
 			step_height: 50, // Px
-			start: '2016-05-15 00:00:00'
+			start: '2016-05-16 00:00:00',
 		},
 		initialize: function() {
+			// Render week headers
+			for (var i = 0;i<7;i++) {
+				var day = moment(this.variables.start).add(i,'days');
+				$(".week-header .column-day:eq("+i+")").html(day.format("ddd DD"));
+			}
+			// Render schedule
 			for (var i = 0;i<24*60/this.variables.step;i++) {
 				var time = moment(this.variables.start).add(i*this.variables.step,'minutes');
 				var step_element = $("<div class='step'>"+time.format("HH:mm")+"</div>");
@@ -17,9 +23,17 @@
 				height: $(".steps").height()+"px"
 			})
 
+			// Fetch data
+
+			$.getJSON("/js/data.json",function(r){
+				console.log("Process me");
+				console.log(r);
+			}).error(function(err){
+				console.log(err);
+			})
 		}
 	}
 
-	Dentalink.initialize();
+	Schedule.initialize();
 
 })(jQuery);
